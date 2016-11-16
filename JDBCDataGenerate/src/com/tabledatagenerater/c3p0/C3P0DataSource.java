c1576a361f479ce2e5e4ef2c9647c6492c28613c
xxm <xxm@DESKTOP-A3LC4Q5>
2016-11-16 10:29:39 +0800

    slqserver的jar包

--- JDBCDataGenerate/src/com/tabledatagenerater/c3p0/C3P0DataSource.java
+++ JDBCDataGenerate/src/com/tabledatagenerater/c3p0/C3P0DataSource.java
@@ -19,18 +19,18 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 public class C3P0DataSource {
 
 	/** driver 驱动 */
-	public String					driver;
+	public String							driver;
 	/** url 数据库链接 */
-	public String					url;
+	public String							url;
 	/** user 帐号 */
-	public String					user;
+	public String							user;
 	/** password 密码 */
-	public String					password;
+	public String							password;
 	/** dataSource C3P0数据源 */
 	private static ComboPooledDataSource	dataSource;
-	/** dsWorkerPool 数据源的工作池*/
-	private ExecutorService dsWorkerPool;
-	
+	/** dsWorkerPool 数据源的工作池 */
+	private ExecutorService					dsWorkerPool;
+
 	public C3P0DataSource(String driver, String url, String user, String password) {
 
 		this.driver = driver;
@@ -49,7 +49,7 @@ public class C3P0DataSource {
 
 		// pool size
 		int maxPoolSize = 2;
-		
+
 		// DataSource init
 		dataSource = new ComboPooledDataSource();
 		dataSource.setDriverClass(driver);
@@ -62,12 +62,12 @@ public class C3P0DataSource {
 		dataSource.setIdleConnectionTestPeriod(60);
 		dataSource.setForceIgnoreUnresolvedTransactions(false);
 		dataSource.setAutoCommitOnClose(false);
-		
+
 		// DataSource WorkerPool init
 		dsWorkerPool = Executors.newFixedThreadPool(maxPoolSize);
 		dsWorkerPool.execute(new ConnectionRunnable());
 	}
-	
+
 	/**
 	 * 关闭数据源
 	 * 
@@ -86,38 +86,38 @@ public class C3P0DataSource {
 	 */
 	public void executeBatch(List<String> sqlLists) {
 		// 参数检查
-		if (null == sqlLists || sqlLists.isEmpty()){
-			return ;
+		if (null == sqlLists || sqlLists.isEmpty()) {
+			return;
 		}
 		// 提交任务
 		dsWorkerPool.submit(new ConnectionRunnable(sqlLists));
 	}
-	
+
 	/**
 	 * 连接的线程接口
 	 * 
 	 * @Author:xxm
-	 * @version $id:ConnectionRunnable.java,v0.1  2016年10月29日 上午11:37:49 xxm Exp$
+	 * @version $id:ConnectionRunnable.java,v0.1 2016年10月29日 上午11:37:49 xxm Exp$
 	 */
 	public class ConnectionRunnable implements Runnable {
-		
-		/** sqls 待执行的SQL*/
-		private List<String> sqls;
-		
-		/** connectionHolder 数据源连接*/
+
+		/** sqls 待执行的SQL */
+		private List<String>			sqls;
+
+		/** connectionHolder 数据源连接 */
 		private ThreadLocal<Connection>	connectionHolder	= new ThreadLocal<Connection>() {
-			@Override
-			protected Connection initialValue() {
-				Connection conn = null;
-				try {
-					conn = dataSource.getConnection();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-				return conn;
-			}															
-		};
-		
+																@Override
+																protected Connection initialValue() {
+																	Connection conn = null;
+																	try {
+																		conn = dataSource.getConnection();
+																	} catch (SQLException e) {
+																		e.printStackTrace();
+																	}
+																	return conn;
+																}
+															};
+
 		public ConnectionRunnable() {
 			super();
 		}
@@ -128,19 +128,19 @@ public class C3P0DataSource {
 
 		@Override
 		public void run() {
-		
+
 			try {
 				// 参数检查
-				if (null == sqls || sqls.isEmpty()){
-					return ;
+				if (null == sqls || sqls.isEmpty()) {
+					return;
 				}
 				// 批量执行SQL
 				executeBachSQL();
-				
+
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
+
 		}
 
 		/**
--- JDBCDataGenerate/src/com/tabledatagenerater/constant/JDBCParamsEnum.java
+++ JDBCDataGenerate/src/com/tabledatagenerater/constant/JDBCParamsEnum.java
@@ -4,18 +4,19 @@ package com.tabledatagenerater.constant;
  * JDBC访问的数据参数
  * 
  * @Author:xxm
- * @version $id:JDBCParamsEnum.java,v0.1  2016年10月29日 上午9:11:23 xxm Exp$
+ * @version $id:JDBCParamsEnum.java,v0.1 2016年10月29日 上午9:11:23 xxm Exp$
  */
 public enum JDBCParamsEnum {
 
-	DRIVER("oracle.jdbc.driver.OracleDriver"),
-	URL("jdbc:oracle:thin:@127.0.0.1:1521:demo"),
-	ACCOUNT("demo"),
-	PWD("12345678");
-	
-	public String value;
+	ORACLE_DRIVER_URL("oracle.jdbc.driver.OracleDriver","jdbc:oracle:thin:@192.168.0.73:1521:orcl"),	
+	MSSQL_DRIVER_URL("com.microsoft.sqlserver.jdbc.SQLServerDriver","jdbc:sqlserver://192.168.0.112:1433;DatabaseName=ipems_dev_if"),
+	ACCOUNT_PWD("huaweipoc", "12345678");
 
-	private JDBCParamsEnum(String value) {
-		this.value = value;
+	public String valueBefore;
+	public String valueAfter;
+	
+	private JDBCParamsEnum(String valueBefore, String valueAfter) {
+		this.valueBefore = valueBefore;
+		this.valueAfter = valueAfter;
 	}
 }
